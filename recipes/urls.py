1d4967eb04ad44116941d8cb6cc855d63461f72e
Rodrigo Abreu <rodrigo.abreu@unicred.com.br>
2025-02-11 21:32:27 -0300

    ajuste na rota para que receba valor dinamico para o id da receita

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from recipes.views import home
+
+from . import views
 
 urlpatterns = [
-    path('', home),
+    path('', views.home),
+    path('recipes/<int:id>/', views.recipe), #dessa forma o django vai entender que quando a rota recipes/1 vai ser chamada, ele vai chamar a função recipe
 ]
+
+#Devemos difinir na configuração da rota o tipo que deve ser aceito na url, nesse caso o tipo inteiro
