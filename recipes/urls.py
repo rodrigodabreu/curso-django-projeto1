18dae0b0878cd49224c10777777c1465aafadb0f
Rodrigo Abreu <rodrigo.abreu@unicred.com.br>
2025-02-17 21:22:39 -0300

    Nome único nas URLs e app_name para Namespace

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = 'recipes'
+
 urlpatterns = [
-    path('', views.home),
-    path('recipes/<int:id>/', views.recipe), #dessa forma o django vai entender que quando a rota recipes/1 vai ser chamada, ele vai chamar a função recipe
+    path('', views.home, name='home'), #dessa forma o django vai entender que quando a rota recipes/ vai ser chamada, ele vai chamar a função home
+    path('recipes/<int:id>/', views.recipe, name='recipe'), #dessa forma o django vai entender que quando a rota recipes/1 vai ser chamada, ele vai chamar a função recipe
 ]
 
 #Devemos difinir na configuração da rota o tipo que deve ser aceito na url, nesse caso o tipo inteiro
