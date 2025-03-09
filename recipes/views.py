25c28b124009359b32a858d492d8b621f196dbad
Rodrigo Abreu <rodrigo.abreu@unicred.com.br>
2025-03-09 18:45:40 -0300

    feat: implementa retorno 404 para categoria não encontrada

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
 from .models import Recipe
+from django.http import Http404
 
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
@@ -12,6 +13,10 @@ def category(request, category_id):
         category__id=category_id #filtra os objetos da categoria que tem o id igual ao id passado na url
         , is_published=True
     ).order_by('-id')
+
+    if not recipes:
+        raise Http404(f'Category with id {category_id} not found 🫤')
+            
     return render(request, "recipes/pages/category.html", context={
         'recipes': recipes,
         'title': f'{recipes.first().category.name} - Category |' 
