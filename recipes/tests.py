25c1d26a14ec0195f1bd2b7b6d450c58de63fd9a
Rodrigo Abreu <rodrigo.abreu@unicred.com.br>
2025-04-13 18:04:23 -0300

    chore: Iniciando com testes no Django com Pytest, Pytest-Django e Unittest

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class RecipeURLsTest(TestCase):
+    def test_um_nao_eh_igual_a_dois(self):
+        assert 1 == 2 , 'Um não é igual a dois'
+    
+    def test_um_igual_a_um(self):
+        assert 1 == 1 , 'Um é igual a um'
